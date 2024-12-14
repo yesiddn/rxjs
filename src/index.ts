92455c25592fa667b3793e8a64b72d45c3530c22
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-12-13 22:04:39 -0500

    Add feedback messages for user progress and game outcome

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { fromEvent, Subject } from "rxjs";
 const WORDS_LIST = require("./wordsList.json");
 
 const letterRows = Array.from(document.querySelectorAll(".letter-row"));
+const messageText = document.querySelector("#message-text");
 
 const onKeyUp$ = fromEvent<KeyboardEvent>(document, "keyup");
 let letterIndex = 0;
@@ -35,24 +36,61 @@ const removeLetter = {
   next: (event: KeyboardEvent) => {
     const pressedKey = event.key;
 
-    if (pressedKey === "Backspace") {
+    if (pressedKey === "Backspace" && letterIndex > 0) {
       letterIndex--;
 
       const letterBox = letterRows[letterRow].children[letterIndex];
       letterBox.textContent = "";
       letterBox.classList.remove("filled-letter");
+
+      userAnswer.pop();
     }
   }
 }
 
 const checkWord = {
   next: (event: KeyboardEvent) => {
-    if (event.key === "Enter") {
-      if (userAnswer.join("") === rightWord) {
-        // en los subject no es necesario pasarle un valor a next, simplemente con emitir una señal es suficiente
-        userWinOrLose.next();
-      }
+    if (event.key !== "Enter") return;
+
+    if (userAnswer.length < rightWord.length && messageText) {
+      messageText.textContent = 'Keep going!';
+      return;
     }
+
+    if (letterRow > 5) {
+      if (messageText) messageText.textContent = 'You lose!';
+      return;
+    }
+
+    for (let i = 0; i < 5; i++) {
+      let letterColor = '';
+      let letterBox = letterRows[letterRow].children[i];
+
+      let letterPosition = rightWord.indexOf(userAnswer[i]);
+
+      if (letterPosition === -1) {
+        letterColor = 'letter-grey';
+      }
+
+      if (letterPosition !== -1 && letterPosition !== i) {
+        letterColor = 'letter-yellow';
+      }
+
+      if (letterPosition === i) {
+        letterColor = 'letter-green';
+      }
+
+      letterBox.classList.add(letterColor);
+    }
+
+    if (userAnswer.join("") === rightWord) {
+      // en los subject no es necesario pasarle un valor a next, simplemente con emitir una señal es suficiente
+      userWinOrLose.next();
+    }
+    
+    letterRow++;
+    letterIndex = 0;
+    userAnswer = [];
   }
 }
 
